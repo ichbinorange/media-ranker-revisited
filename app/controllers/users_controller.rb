ba153c3db0a963f8c819fa3ff13cec5b8c133dd7
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-11-26 13:01:51 -0800

    add login/logout methods in user controller and auth_builder in user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :auth_hash, only: [:create]
   def index
     @users = User.all
   end
@@ -9,35 +10,38 @@ class UsersController < ApplicationController
   end
 
   def login_form
+    # for view
   end
 
-  def login
-    username = params[:username]
-    if username and user = User.find_by(username: username)
-      session[:user_id] = user.id
-      flash[:status] = :success
-      flash[:result_text] = "Successfully logged in as existing user #{user.username}"
+  def create
+    user = User.find_by(uid: auth_hash[:uid], provider: "github")
+    
+    if user
+      flash[:success] = "Logged in as returning user #{user.name}"
     else
-      user = User.new(username: username)
+      @user = User.build_from_github(auth_hash)
       if user.save
-        session[:user_id] = user.id
-        flash[:status] = :success
-        flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
+        flash[:success] = "Logged in as new user #{user.name}"
       else
-        flash.now[:status] = :failure
-        flash.now[:result_text] = "Could not log in"
-        flash.now[:messages] = user.errors.messages
-        render "login_form", status: :bad_request
-        return
+        flash[:error] = "Could not create new user account: #{user.errors.messages}"
+        return redirect_to root_path
       end
     end
-    redirect_to root_path
+
+    session[:user_id] = user.id
+    return redirect_to root_path
   end
 
-  def logout
+  def destroy
     session[:user_id] = nil
-    flash[:status] = :success
-    flash[:result_text] = "Successfully logged out"
+    flash[:success] = "Successfully logged out!"
+
     redirect_to root_path
+    return
+  end
+
+  protected
+  def auth_hash
+    request.env["omniauth.auth"]
   end
 end
