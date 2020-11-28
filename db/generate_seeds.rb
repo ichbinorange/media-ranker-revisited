cb17a6263611e3f366a49eaf2738ac325a1ecdcd
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-11-28 00:15:40 -0800

    update seeds data for work belongs to user

--- db/generate_seeds.rb
+++ db/generate_seeds.rb
@@ -11,14 +11,27 @@ require "csv"
 # doesn't currently check for if titles are unique against each other
 
 CSV.open("db/media_seeds.csv", "w", :write_headers => true,
-                                    :headers => ["category", "title", "creator", "publication_year", "description"]) do |csv|
+                                    :headers => ["category", "title", "creator", "publication_year", "description", "user_id"]) do |csv|
   25.times do
     category = %w(album book).sample
     title = Faker::Coffee.blend_name
     creator = Faker::Name.name
     publication_year = rand(Date.today.year - 100..Date.today.year)
     description = Faker::Lorem.sentence
+    user_id = rand(1..10)
 
-    csv << [category, title, creator, publication_year, description]
+    csv << [category, title, creator, publication_year, description, user_id]
+  end
+end
+
+CSV.open("db/users_seeds.csv", "w", :write_headers => true,
+         :headers => ["username",  "uid", "provider", "email"]) do |csv|
+  10.times do
+    name = Faker::Name.name
+    uid = "#{rand(10000000..99999999)}"
+    provider = "github"
+    email = Faker::Internet.email
+
+    csv << [name, uid, provider, email]
   end
 end
